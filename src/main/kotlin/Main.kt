75f3428d3c485376646957df4ae4b9de3f30eda5
Diego Cano Sibón <dcsibon@gmail.com>
2024-01-24 19:08:45 +0100

    Versión 3 corrección de errores encontrados en clase de DAM1.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -29,7 +29,7 @@ Implementa, al menos, lo siguiente:
 - Clase enumerada Color, que contendrá los colores disponibles: blanco, negro, gris, azul y verde.
 
 En el programa principal se deben crear 3 cafeteras de capacidad 1000, 750 y 500 c.c. con 0, 750 y 200 c.c de cantidad respectivamente. Para crear cada cafetera, debéis utilizar un constructor diferente.
-También debéis generar una lista de 20 tazas con la capacidad de 50, 75 y 100 de manear aleatoria.
+También debéis generar una lista de 20 tazas con la capacidad de 50, 75 y 100 de manera aleatoria.
 Implementar el resto de comentarios TODO de la función main().
 */
 
